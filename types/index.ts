800df4711d8a05b24ac7f999edb3dab4e4993280
Furkan Ozbek <abdullah.furkan.ozbek@gmail.com>
2023-07-23 16:12:33 +0300

    types added

---
+++ types/index.ts
@@ -0,0 +1,31 @@
+export interface Card {
+  id: string;
+  cardType: CardTypes;
+  face: string;
+}
+
+export enum PlayerType {
+  Dealer = "Dealer",
+  Player = "Player",
+}
+
+export enum GameStatus {
+  Started = "Start",
+  DealerStayed = "DealerStayed",
+  PlayerStayed = "PlayerStayed",
+  DealerBusted = "DealerBusted",
+  PlayerBusted = "PlayerBusted",
+  PlayerWon = "PlayerWon",
+  DealerWon = "DealerWon",
+  CalculateWinner = "CalculateWinner",
+  Empty = "Empty",
+  Draw = "Draw",
+  Finished = "Finished",
+}
+
+export enum CardTypes {
+  Diamonds = "Diamonds",
+  Clubs = "Clubs",
+  Hearts = "Hearts",
+  Spades = "Spades",
+}
